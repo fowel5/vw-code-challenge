360d218bda33587680a5051645aa9e5e176101b6
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-06-27 20:19:16 +0200

    feat: create header and footer components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className='bg-white w-full h-[10vh] pt-4 flex justify-center align-middle'>
+      <img src='/header-image.png' width={200} height={80} />
+    </div>
+  );
+}
