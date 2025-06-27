5f6b5dcc4bc752dbfe4db0b64f2ea883c773c1bd
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-06-27 14:43:13 +0200

    chore: adapt tests

--- src/components/DataTable/DataTable.test.tsx
+++ src/components/DataTable/DataTable.test.tsx
@@ -5,14 +5,14 @@ vi.mock('../../hooks/useStudents', () => ({
   useStudents: () => ({
     students: [
       {
-        id: 1,
+        id: '1',
         firstName: 'Alice',
         lastName: 'Smith',
         email: 'alice@example.com',
         mark: 7.3,
       },
       {
-        id: 2,
+        id: '2',
         firstName: 'Bob',
         lastName: 'Johnson',
         email: 'bob@example.com',
