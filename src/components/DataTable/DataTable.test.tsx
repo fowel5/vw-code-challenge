25dd14e28ba6f557a9acad40d939eff1441dbfb7
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-06-27 12:39:32 +0200

    chore: adapt test

--- src/components/DataTable/DataTable.test.tsx
+++ src/components/DataTable/DataTable.test.tsx
@@ -1,33 +1,29 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import DataTable from './DataTable';
 
-describe('BeautifulTable', () => {
-  beforeEach(() => {
-    globalThis.fetch = vi.fn().mockResolvedValue({
-      ok: true,
-      json: async () => [
-        {
-          id: 1,
-          firstName: 'Alice',
-          lastName: 'Smith',
-          email: 'alice@example.com',
-          mark: 7.3,
-        },
-        {
-          id: 2,
-          firstName: 'Bob',
-          lastName: 'Johnson',
-          email: 'bob@example.com',
-          mark: 2.4,
-        },
-      ],
-    });
-  });
+vi.mock('../../hooks/useStudents', () => ({
+  useStudents: () => ({
+    students: [
+      {
+        id: 1,
+        firstName: 'Alice',
+        lastName: 'Smith',
+        email: 'alice@example.com',
+        mark: 7.3,
+      },
+      {
+        id: 2,
+        firstName: 'Bob',
+        lastName: 'Johnson',
+        email: 'bob@example.com',
+        mark: 2.4,
+      },
+    ],
+    setStudents: vi.fn(),
+  }),
+}));
 
-  afterEach(() => {
-    vi.resetAllMocks();
-  });
-
+describe('DataTable', () => {
   it('renders table headers', async () => {
     render(<DataTable />);
     expect(await screen.findByText('ID')).toBeInTheDocument();
