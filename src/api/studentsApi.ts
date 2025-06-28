9832dc62ad374902fa42299eed39d7afb75f42ab
fowel5 <134442783+fowel5@users.noreply.github.com>
2025-06-28 12:14:28 +0200

    fix: change jsonserver version and add create function VW14 (#14)
    * fix: downgrade jsonserver version
    
    * feat: add createStudent function

--- src/api/studentsApi.ts
+++ src/api/studentsApi.ts
@@ -3,3 +3,15 @@ import type { Student } from '../types/Student';
 export async function fetchStudents(): Promise<Student[]> {
   return fetch('http://localhost:3000/students').then((res) => res.json());
 }
+
+export async function createStudent(
+  newStudent: Omit<Student, 'id'>
+): Promise<Student> {
+  return fetch('http://localhost:3000/students', {
+    body: JSON.stringify(newStudent),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => res.json());
+}
