4bb0db3ef8196ae9bf0db2b215d404fd37295d1c
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-06-28 13:41:02 +0200

    feat: add new button which calls the create student form

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -2,17 +2,16 @@ import { useState } from 'react';
 import { searchStudent } from '../../utils/filters';
 import { useStudents } from '../../hooks/useStudents';
 import { useNavigate } from 'react-router';
+import { createPortal } from 'react-dom';
+import Overlay from '../Overlay/Overlay';
+import CreateStudentForm from '../Overlay/CreateStudentForm';
 
 export default function DataTable() {
   const [search, setSearch] = useState('');
+  const [showOverlay, setShowOverlay] = useState(false);
+
   const studentsContext = useStudents();
   const navigate = useNavigate();
-
-  if (studentsContext === undefined) {
-    navigate('/error');
-
-    return;
-  }
 
   const { students } = studentsContext;
   const filteredData = searchStudent(students, search);
@@ -20,14 +19,26 @@ export default function DataTable() {
   return (
     <div className='p-6 max-w-full h-[80vh]'>
       <h1 className='text-2xl font-bold mb-4 text-center'>Students Overview</h1>
-      <input
-        type='text'
-        placeholder='Search users...'
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        className='mb-4 w-full max-w-md mx-auto block px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500'
-      />
-      <div className='overflow-x-auto overflow-y-scroll rounded-2xl shadow-md'>
+      <div className='flex items-center gap-4 mb-4 w-full flex-col sm:flex-row'>
+        <div>
+          <button
+            className='bg-[#00A5A8] hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg shadow transition-colors duration-200 cursor-pointer'
+            onClick={() => setShowOverlay(true)}
+          >
+            + New Student
+          </button>
+        </div>
+        <div className='flex-1 flex justify-end'>
+          <input
+            type='text'
+            placeholder='Search students...'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className='w-full max-w-md px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500'
+          />
+        </div>
+      </div>
+      <div className='overflow-x-auto overflow-y-scroll rounded-2xl shadow-md max-h-[65vh]'>
         <table className='min-w-full w-auto divide-gray-200'>
           <thead className='bg-gray-100 sticky top-0'>
             <tr>
@@ -81,6 +92,21 @@ export default function DataTable() {
           </tbody>
         </table>
       </div>
+      {showOverlay &&
+        createPortal(
+          <Overlay
+            onClose={() => {
+              setShowOverlay(false);
+            }}
+          >
+            <CreateStudentForm
+              onClose={() => {
+                setShowOverlay(false);
+              }}
+            />
+          </Overlay>,
+          document.body
+        )}
     </div>
   );
 }
