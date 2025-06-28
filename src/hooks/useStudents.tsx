476bdd4ca2c26aebd406f6d843f7084c2a8cf0c3
fowel5 <134442783+fowel5@users.noreply.github.com>
2025-06-28 13:58:01 +0200

    feat: implement form to create new student VW15 (#15)
    * feat: handle error in fetching within the hook leading to error page
    
    * feat: create reusable overlay component
    
    * feat: add basic student form component
    
    * feat: add new button which calls the create student form
    
    * fix: do not reload page when db.json changes
    
    * chore: comment
    
    * chore: adapt tests

--- src/hooks/useStudents.tsx
+++ src/hooks/useStudents.tsx
@@ -1,14 +1,19 @@
 import { useContext } from 'react';
 import { StudentContext } from '../logic/StudentsProvider';
 import type { Student } from '../types/Student';
+import { useNavigate } from 'react-router';
 
-export function useStudents():
-  | { students: Student[]; setStudents(students: Student[]): void }
-  | undefined {
+export function useStudents(): {
+  students: Student[];
+  setStudents(students: Student[]): void;
+} {
   const studentContextValue = useContext(StudentContext);
+  const navigate = useNavigate();
 
   if (studentContextValue === undefined) {
-    return undefined;
+    navigate('/error');
+
+    return { students: [], setStudents: () => {} };
   }
 
   return studentContextValue;
