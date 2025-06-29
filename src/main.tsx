13f2de1bbab96b194faca938577c59cde02d1eed
fowel5 <134442783+fowel5@users.noreply.github.com>
2025-06-29 16:05:47 +0200

    feat: create layoutwrapper component VW28 (#28)
    * feat: create layout wrapper component
    With that, we dont need to set the height, paddings and widths in order to make the page look good
    
    * feat: use LayoutWrapper to wrapp application
    
    * feat: delete useless css

--- src/main.tsx
+++ src/main.tsx
@@ -10,21 +10,24 @@ import Footer from './components/Footer/Footer';
 import Error from './components/Error/Error';
 import PageNotFound from './components/Error/PageNotFound';
 import StudentForm from './components/Forms/StudentForm';
+import LayoutWrapper from './components/LayoutWrapper/LayoutWrapper';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
-    <StudentsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<DataTable />} />
-          <Route path='/student/:id' element={<StudentShowCase />} />
-          <Route path='/student/:id/edit' element={<StudentForm onClose={() => {}} />} />
-          <Route path='/error' element={<Error />} />
-          <Route path='/*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </StudentsProvider>
+    <LayoutWrapper>
+      <StudentsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<DataTable />} />
+            <Route path='/student/:id' element={<StudentShowCase />} />
+            <Route path='/student/:id/edit' element={<StudentForm onClose={() => {}} />} />
+            <Route path='/error' element={<Error />} />
+            <Route path='/*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </StudentsProvider>
+    </LayoutWrapper>
     <Footer />
   </StrictMode>
 );
