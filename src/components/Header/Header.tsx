f168cbff6e704a959b4258e8ef62fc3f7188a85e
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-06-29 10:40:19 +0200

    feat: render container in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,22 @@
+import { Bounce, ToastContainer } from 'react-toastify';
+
 export default function Header() {
   return (
     <div className='bg-white w-full h-[10vh] pt-4 flex justify-center align-middle'>
       <img src='/header-image.png' width={220} />
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+        transition={Bounce}
+      />
     </div>
   );
 }
