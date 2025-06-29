bb8321cdcb338b5760e3512f5d894b56a4079712
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-06-29 12:41:51 +0200

    feat: add delete and update api functions

--- src/api/studentsApi.ts
+++ src/api/studentsApi.ts
@@ -13,3 +13,22 @@ export async function createStudent(newStudent: Omit<Student, 'id'>): Promise<St
     },
   }).then((res) => res.json());
 }
+
+export async function updateStudent(studentToUpdate: Student): Promise<Student> {
+  return fetch(`http://localhost:3000/students/${studentToUpdate.id}`, {
+    body: JSON.stringify(studentToUpdate),
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => res.json());
+}
+
+export async function deleteStudent(id: number): Promise<void> {
+  return fetch(`http://localhost:3000/students/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((res) => res.json());
+}
