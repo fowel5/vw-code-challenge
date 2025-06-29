3eb4ed811014e1193a44988d007db5fd505dca1b
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-06-29 14:13:41 +0200

    feat: add basic config for cypress

--- eslint.config.js
+++ eslint.config.js
@@ -9,16 +9,12 @@ export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    extends: [js.configs.recommended, tseslint.configs.recommended, reactHooks.configs['recommended-latest'], reactRefresh.configs.vite],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+
     rules: {
       'no-console': 'error',
       'newline-before-return': 'error',
