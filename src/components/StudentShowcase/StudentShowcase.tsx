e49408506db13cd5f509ac077c10751626faf917
Miguel <miguel.angel.beltra.mayoral@hotmail.com>
2025-06-29 10:25:33 +0200

    feat: use new forms in showase + design fixes

--- src/components/StudentShowcase/StudentShowcase.tsx
+++ src/components/StudentShowcase/StudentShowcase.tsx
@@ -1,9 +1,16 @@
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import { useStudents } from '../../hooks/useStudents';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import Overlay from '../Overlay/Overlay';
+import { DeleteForm } from '../Overlay/DeleteForm';
 
 export default function StudentShowCase() {
   const { id } = useParams();
   const studentContext = useStudents();
+  const [isDelete, setIsDelete] = useState(false);
+  const navigate = useNavigate();
+
   const studentIdToSearch = parseInt(id || '0');
   if (studentContext === undefined) {
     return undefined;
@@ -19,10 +26,21 @@ export default function StudentShowCase() {
   return (
     <div className='p-6 max-w-auto h-[80vh]'>
       <h2 className='text-2xl font-bold mb-4 text-center'>Student Details</h2>
+      <div className='flex justify-center mb-4 gap-4'>
+        <button
+          className='bg-[#00A5A8] hover:bg-[rgba(0,165,168,0.8)] text-white font-semibold px-4 py-2 rounded-lg shadow transition-colors duration-200 cursor-pointer'
+          onClick={() => navigate(`/student/${id}/edit`)}
+        >
+          Edit
+        </button>
+        <button className='bg-red-600 hover:opacity-75 text-white font-semibold px-4 py-2 rounded-lg shadow transition-opacity duration-200 cursor-pointer' onClick={() => setIsDelete(true)}>
+          Delete
+        </button>
+      </div>
       <div className='flex justify-center'>
         <div className='overflow-x-auto rounded-2xl shadow-md w-full max-w-lg'>
           <table className='min-w-full w-auto divide-y divide-gray-200'>
-            <thead className='bg-[rgba(0,165,168,0.5)]'>
+            <thead className='bg-[#00A5A8]'>
               <tr>
                 <th className='px-6 py-3 text-left text-sm font-medium text-gray-700'>Field</th>
                 <th className='px-6 py-3 text-left text-sm font-medium text-gray-700'>Value</th>
@@ -53,6 +71,13 @@ export default function StudentShowCase() {
           </table>
         </div>
       </div>
+      {isDelete &&
+        createPortal(
+          <Overlay onClose={() => setIsDelete(false)}>
+            <DeleteForm student={studentToShow} onClose={() => setIsDelete(false)} />
+          </Overlay>,
+          document.body
+        )}
     </div>
   );
 }
